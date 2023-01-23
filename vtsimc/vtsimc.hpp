7b97e165cde1793599b1421296dc4d62f0b27650
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2023-01-23 15:51:09 +0900

    Update vtsimc.hpp

--- vtsimc/vtsimc.hpp
+++ vtsimc/vtsimc.hpp
@@ -249,6 +249,12 @@ public:
                     t0[tn[i_tn_ac[i]].i1] = pre_t;
                 }
                 else{       //設定温度を満たしていれば最大能力を超えているかチェック & 除湿による吹き出し口絶対湿度の計算
+                    if(tn[i_tn_ac[i]].ac_model == None){
+                        double  x_in  = sn[tn[i_tn_ac[i]].i1].x[ts], x_out = sn[tn[i_tn_ac[i]].i2].x[ts];
+                        double  qv    = vn[i_vn_ac[i]].qv[ts], To = tn[i_tn_ac[i]].To[ts], ho = tn[i_tn_ac[i]].ho[ts];
+                        double  t_out_lim, x_out_lim;
+                        tie(t_out_lim, x_out_lim) = tn[i_tn_ac[i]].get_None_hs_out(t_in, t_out, x_in, x_out, qv, To, ho, ts);
+                    }
                     if(tn[i_tn_ac[i]].ac_model == AC_RAC){
                         double  x_in  = sn[tn[i_tn_ac[i]].i1].x[ts], x_out = sn[tn[i_tn_ac[i]].i2].x[ts];
                         double  qv    = vn[i_vn_ac[i]].qv[ts], To = tn[i_tn_ac[i]].To[ts], ho = tn[i_tn_ac[i]].ho[ts];
