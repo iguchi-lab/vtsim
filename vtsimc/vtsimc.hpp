568f35b96bdef27f758d4cde1be533ff05e52196
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2024-02-01 21:29:05 +0900

    Update vtsimc.hpp

--- vtsimc/vtsimc.hpp
+++ vtsimc/vtsimc.hpp
@@ -291,10 +291,10 @@ public:
                             tie(t_out_lim, x_out_lim) 
                                 = tn[i_tn_ac[i]].get_DUCT_C_hs_out(t_in, t_out, x_in, x_out, qv, To, ho, ts);
                             
-                            if(t_out - t_out_lim > 0.001){
-                                tn[i_tn_ac[i]].pre_tmp[ts] -= 0.01;
-                                t0[tn[i_tn_ac[i]].i1]      -= 0.01;
-                            }
+                            //if(t_out - t_out_lim > 0.001){
+                            //    tn[i_tn_ac[i]].pre_tmp[ts] -= 0.01;
+                            //    t0[tn[i_tn_ac[i]].i1]      -= 0.01;
+                            //}
                             
                         }
                         if(t_in > t_out){
